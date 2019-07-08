7231bb4622842076611df7874524eed1b8698fc5
“Ohene <oheneb.bonsu@gmail.com>
2019-07-08 17:10:31 +0000

    Showing Random Quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,6 @@
 class QuotesController < ApplicationController
     def index
+      @quote = Quote.order("RANDOM()").first 
+      #something is wrong her. Quote is italicized. Even when you modify it to say something absurd, it still pulls up Einstein quote
     end
 end
