0b8014be8c348eb3ce09d88e3fade2c5ac3e4e20
Ohene Bonsu <oheneb.bonsu@gmail.com>
2019-07-11 15:47:21 +0000

    Lesson 12:Error when attempting to run generator

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,4 +3,7 @@ class QuotesController < ApplicationController
       @quote = Quote.order("RANDOM()").first 
       #something is wrong her. Quote is italicized. Even when you modify it to say something absurd, it still pulls up Einstein quote
     end
+
+    def new
+    end
 end
