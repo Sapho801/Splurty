67ae06c0f9f484a2cddc1a700b9c37cccb9cc0dc
Ohene Bonsu <oheneb.bonsu@gmail.com>
2019-07-12 18:42:05 +0000

    Completed Lesson 17:Finishing Touches

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,9 +4,7 @@ class QuotesController < ApplicationController
       #something is wrong her. Quote is italicized. Even when you modify it to say something absurd, it still pulls up Einstein quote
     end
 
-    def new
-      @quote = Quote.new
-    end
+    
 
     def create 
       @quote = Quote.create(quote_params) 
